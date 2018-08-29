3ec5b75dba7615cb3a1574870d22d0f55577d151
Gil Gald <gil.2006@gmail.com>
2018-08-29 18:49:43 +0300

    a

--- tests/test_get_subs.sh
+++ tests/test_get_subs.sh
@@ -2,7 +2,7 @@
 
 x=$(az account list )
 length=$(jq -n "$x" | jq '. | length')
-$index =0
+$index=0
 END=$length-1
 i=0
 subscription_number=0
@@ -14,11 +14,11 @@ do
 	echo "$((i+1))" $(jq -n "$x" | jq .["$i"].name )  $(jq -n "$x" | jq .["$i"].id )
 	((i++))
 done
-echo "Enter number between 1 to " $length
 
+echo "Enter number between 1 to " $length
 read subscription_number
 
-while [ $subscription_number -lt 1 -o $subscription_number -gt $length ]
+while [  $subscription_number -lt 1 -o $subscription_number -gt $length ]
 do
 	echo "Please enter number between 1 to " $length
 	read subscription_number
