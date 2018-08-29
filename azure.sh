aed2d5151b004dab10bcda4cef961105ee43932c
Gil Gald <gil.2006@gmail.com>
2018-08-29 17:44:24 +0300

    fix

--- azure.sh
+++ azure.sh
@@ -73,7 +73,7 @@ fi
 
 echo -e "\n\n\n-------------------------------------------------------------------------"
 echo "Copy the text below and paste it into Colony's Azure authentication page"
-echo -e "appId=$AppId,\nappKey=$AppKey,\ntenantId=$TenantId,\nsubscriptionId=$SubscriptionId,\ncolonyResourceGroup=$ColonyMgmtRG"
+echo -e "appId:$AppId,\nappKey:$AppKey,\ntenantId:$TenantId,\nsubscriptionId:$SubscriptionId,\ncolonyResourceGroup:$ColonyMgmtRG"
 echo -e "-------------------------------------------------------------------------\n\n"
-echo "appId=$AppId,appKey=$AppKey,tenantId=$TenantId,subscriptionId=$SubscriptionId,colonyResourceGroup=$ColonyMgmtRG" > colony_data.txt
+echo "appId:$AppId,appKey:$AppKey,tenantId:$TenantId,subscriptionId:$SubscriptionId,colonyResourceGroup:$ColonyMgmtRG" > colony_data.txt
 echo "Done"
