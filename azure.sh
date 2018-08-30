dcd28a97dded2594530c97bcd35307dcb9643d65
Gil Gald <gil.2006@gmail.com>
2018-08-30 14:28:12 +0300

    added subscriptiona nd lock on resource group

--- azure.sh
+++ azure.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-echo -e "Preparing integration parameters"
+echo -e "   ___ _    ___  _   _ ___  ___ _  _ ___ _    _        ___ ___  _    ___  _  ___   __"
+echo -e "  / __| |  / _ \| | | |   \/ __| || | __| |  | |  ___ / __/ _ \| |  / _ \| \| \ \ / /"
+echo -e " | (__| |_| (_) | |_| | |) \__ \ __ | _|| |__| |_|___| (_| (_) | |_| (_) | .   \ V / "
+echo -e "  \___|____\___/ \___/|___/|___/_||_|___|____|____|   \___\___/|____\___/|_|\_| |_|  "
 
+#========================================================================================
+x=$(az account list)
+length=$(jq -n "$x" | jq '. | length')
+END=$length-1
+i=0
+declare -i subscription_number=0
+
+echo "Please type subscription number:#"
+while [[ $i -le $END ]]
+do
+	# prints subscription name and id
+	echo "$((i+1))" $(jq -n "$x" | jq .["$i"].name )  $(jq -n "$x" | jq .["$i"].id )
+	((i++))
+done
+
+read -p  "Please enter number between 1 to $length: " subscription_number
+
+while [  $subscription_number -lt 1 -o $subscription_number -gt $length ]
+do	
+	read -p "Please enter number between 1 to $length: " subscription_number
+done
+SubscriptionId=$(jq -n "$x" | jq .["$((subscription_number-1))"].id -r)
+
+echo -e "Chosen subscription:" $GREEN$(jq -n "$x" | jq .["$((subscription_number-1))"].name )  $SubscriptionId$NC
+
+#========================================================================================
 #creting a random key
 COLONY_RANDOM=$(date +%s | sha256sum | base64 | head -c 12;echo)$(echo $RANDOM)
 COLONY_RANDOM="$(echo $COLONY_RANDOM | tr '[A-Z]' '[a-z]')"
 AppName=$(echo "COLONY-"$COLONY_RANDOM)
-
 ColonyMgmtRG=$(echo "colony"$COLONY_RANDOM)
 StorageName=$(echo "colony"$COLONY_RANDOM)
 CosmosDbName=$(echo ""$ColonyMgmtRG"-sandbox-db")
 AppKey=$(openssl rand -base64 32)
 TenantId=$(az account show --query tenantId -o tsv)
-SubscriptionId=$(az account show --query id -o tsv)
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
 
 REGION="westeurope"
 if [ ! -z "$1" ]
 then
       REGION=$1
 fi
-echo -e "Resources will be created under $GREEN$REGION$NC region"
 
+echo -e "All resources will be created under $GREEN$REGION$NC region"
 echo -e "Creating AD application for CloudShell Colony"
 az ad sp create-for-rbac -n $AppName --password $AppKey
 AppId=$(az ad app list --display-name $AppName | jq '.[0].appId' | tr -d \")
@@ -34,23 +60,26 @@ bash -c "cat >> role.json" <<EOL
 [{"resourceAppId": "797f4846-ba00-4fd7-ba43-dac1f8f63013","resourceAccess":[{"id": "41094075-9dad-400e-a0bd-54e686782033", "type":"Scope"}]}]
 EOL
  
-az ad app update --id $AppId --required-resource-accesses role.json
+az ad app update --id $AppId --required-resource-accesses role.json --subscription $SubscriptionId
 rm role.json
-echo -e "\n\nApplication Name = $AppName \nApplication ID = $AppId \nApplication Key = $AppKey \nTenant ID = $TenantId \nSubscription ID = $SubscriptionId"
+echo -e "\n\nApplication Name : $AppName \nApplication ID : $AppId \nApplication Key : $AppKey \nTenant ID : $TenantId \nSubscription ID : $SubscriptionId"
+
+#========================================================================================
+
 
 #1.create resource group:
 echo -e "$GREEN---Creating colony resource group (1/3) "$ColonyMgmtRG$NC
-az group create -l $REGION -n $ColonyMgmtRG
+az group create -l $REGION -n $ColonyMgmtRG --subscription $SubscriptionId --tags colony-mgmt-group=''
 echo "---Verifing Resource group exists "$ColonyMgmtRG 
 
-if [ ! "$(az group exists -n $ColonyMgmtRG)" = "true" ]; then
+if [ ! "$(az group exists -n $ColonyMgmtRG --subscription $SubscriptionId)" = "true" ]; then
         echo "Error resource group does not exists" 
         exit 1
 fi
 
 #2.Create the storage account:
 echo -e "$GREEN---Creating storage account (2/3) "$StorageName$NC
-az storage account create -n $StorageName -g $ColonyMgmtRG -l $REGION --sku Standard_LRS  --kind StorageV2 --tags colony-mgmt-storage=''
+az storage account create -n $StorageName -g $ColonyMgmtRG -l $REGION --sku Standard_LRS  --kind StorageV2 --tags colony-mgmt-storage='' --subscription $SubscriptionId
 echo "---Verifing storage account exists "$StorageName 
 
 #if storage account name is available it means that it was not created
@@ -59,10 +88,9 @@ if [ "$(az storage account check-name -n $StorageName -o json | jq -r .nameAvail
         exit 1
 fi
 
-
 #3.Create mongo API cosmos db:
-echo -e "${GREEN}---Creating CosmosDB (3/3) "$CosmosDbNames$NC
-az cosmosdb create -g $ColonyMgmtRG -n $CosmosDbName --kind MongoDB
+echo -e "${GREEN}---Creating CosmosDB ${RED}This step may take a few minutes ${GREEN}(3/3)${NC} "$CosmosDbNames$NC
+az cosmosdb create -g $ColonyMgmtRG -n $CosmosDbName --kind MongoDB --subscription $SubscriptionId --tags colony-mgmt-cosmosdb=''
 
 echo "---Verifing CosmosDB exists "$CosmosDbName 
 #if storage account name is available it means that it was not created
@@ -71,9 +99,15 @@ if [ ! "$(az cosmosdb check-name-exists -n $CosmosDbName)" = "true" ]; then
         exit 1
 fi
 
+echo "---Locking Resource Group "$ColonyMgmtRG 
+az lock create -g $ColonyMgmtRG -n LockGroup --lock-type CanNotDelete --subscription $SubscriptionId
+
 echo -e "\n\n\n-------------------------------------------------------------------------"
 echo "Copy the text below and paste it into Colony's Azure authentication page"
-echo -e "appId:$AppId,\nappKey:$AppKey,\ntenantId:$TenantId,\nsubscriptionId:$SubscriptionId,\ncolonyResourceGroup:$ColonyMgmtRG"
+echo -e "${GREEN}appId:$AppId,\nappKey:$AppKey,\ntenantId:$TenantId,\nsubscriptionId:$SubscriptionId,\ncolonyResourceGroup:$ColonyMgmtRG${NC}"
 echo -e "-------------------------------------------------------------------------\n\n"
-echo "appId:$AppId,appKey:$AppKey,tenantId:$TenantId,subscriptionId:$SubscriptionId,colonyResourceGroup:$ColonyMgmtRG" > colony_data.txt
+
+
+
+                                                                                     
 echo "Done"
